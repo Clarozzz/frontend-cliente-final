cb98e8190dbe181627c8ec4f56cfa4f8d39f6399
Cesar <jcsdlg49@gmail.com>
2022-11-09 20:54:53 -0600

    Se agrego el apartado de inicio de sesion

---
+++ js/controladorLP.js
@@ -0,0 +1,7 @@
+
+function inicioSesion() {
+    document.getElementById('navbar-inicio').style.display = 'none';
+    document.getElementById('pagina-inicio').style.display = 'none';
+    document.getElementById('apartado-inicio-sesion').style.display = 'block';
+    document.body.style.backgroundColor = 'white';
+}
