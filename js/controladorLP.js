5ea2fd53d29c5cfd8387c9dbcfc5c71daaef2a52
Cesar <jcsdlg49@gmail.com>
2022-11-10 12:57:31 -0600

    Se crea la pagina de los clientes

--- js/controladorLP.js
+++ js/controladorLP.js
@@ -2,6 +2,27 @@
 function inicioSesion() {
     document.getElementById('navbar-inicio').style.display = 'none';
     document.getElementById('pagina-inicio').style.display = 'none';
+    document.getElementById('apartado-registro').style.display = 'none';
     document.getElementById('apartado-inicio-sesion').style.display = 'block';
     document.body.style.backgroundColor = 'white';
+}
+
+function volverPaginaInicio() {
+    document.getElementById('navbar-inicio').style.display = 'block';
+    document.getElementById('pagina-inicio').style.display = 'block';
+    document.getElementById('apartado-inicio-sesion').style.display = 'none';
+    document.getElementById('apartado-registro').style.display = 'none';
+    document.body.style.backgroundColor = '#FFAF54';
+}
+
+function abrirRegistro() {
+    document.getElementById('navbar-inicio').style.display = 'none';
+    document.getElementById('pagina-inicio').style.display = 'none';
+    document.getElementById('apartado-registro').style.display = 'block';
+    document.getElementById('apartado-inicio-sesion').style.display = 'none';
+    document.body.style.backgroundColor = 'white';
+}
+
+function abrirClientes() {
+    window.open('clientes.html', '_self');
 }
