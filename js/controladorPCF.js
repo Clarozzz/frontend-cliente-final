4d4f09b68ec8873525b49c71f918688b464f7a73
Cesar <jcsdlg49@gmail.com>
2022-11-12 19:07:56 -0600

    Prototipo de la pagina del cliente finalizado

--- js/controladorPCF.js
+++ js/controladorPCF.js
@@ -42,7 +42,7 @@ var categorias = [
                         nombreProducto: "Coca cola 500ml",
                         descripcion: "Coca cola sabor original",
                         precio: 15,
-                        icono: "../img/cocacolaBotella.jpg"
+                        icono: "../img/cocacolaBotella.png"
                     }
                 ]
             },
@@ -54,7 +54,7 @@ var categorias = [
                         nombreProducto: "Pepsi 500ml",
                         descripcion: "Pepsi sabor original",
                         precio: 15,
-                        icono: "../img/pepsiBotella.jpg"
+                        icono: "../img/pepsiBotella.png"
                     }
                 ]
             }
@@ -82,6 +82,7 @@ generarCategorias();
 function categoriaSeleccionada(indice) {
     document.getElementById('paginaCategorias').style.display = 'none';
     document.getElementById('paginaProductos').style.display = 'none';
+    document.getElementById('carrito').style.display = 'none';
     document.getElementById('paginaEmpresas').style.display = 'block';
 
     console.log(indice)
@@ -106,21 +107,103 @@ function regresarACategorias() {
     document.getElementById('paginaCategorias').style.display = 'block';
     document.getElementById('paginaProductos').style.display = 'none';
     document.getElementById('paginaEmpresas').style.display = 'none';
+    document.getElementById('carrito').style.display = 'none';
+    document.getElementById('iconoCarrito').style.display = 'block';
 }
 
 function regresarAEmpresas() {
     document.getElementById('paginaCategorias').style.display = 'none';
     document.getElementById('paginaProductos').style.display = 'none';
     document.getElementById('paginaEmpresas').style.display = 'block';
+    document.getElementById('carrito').style.display = 'none';
 }
 
 function empresaSeleccionada(indice) {
     document.getElementById('paginaCategorias').style.display = 'none';
     document.getElementById('paginaProductos').style.display = 'block';
     document.getElementById('paginaEmpresas').style.display = 'none';
+    document.getElementById('carrito').style.display = 'none';
 
     console.log(indice)
     seleccionEmpresa = seleccionCategoria.empresas[indice];
+    document.getElementById('nombreEmpresa').innerHTML = `${seleccionEmpresa.nombreEmpresa}`
+    cargarProductos();
+}
 
-    console.log(seleccionEmpresa)
+function cargarProductos() {
+    document.getElementById('productos').innerHTML = '';
+
+    seleccionEmpresa.productos.forEach((producto, indice) => {
+        document.getElementById('productos').innerHTML += 
+        `
+        <div class="tamano-opcion mt-4 border rounded-4 p-3 borde-color-primario">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <h3 class="fs-4">${producto.nombreProducto}</h3>
+                        <p>${producto.descripcion}</p>
+                    </div>
+                    <div>
+                        <img src="${producto.icono}" alt="${producto.nombreProducto}" width="70">
+                    </div> 
+                </div>
+                <div class="d-flex justify-content-between mt-2">
+                    <div>
+                        <h3>Lps. ${producto.precio}</h3>
+                    </div>
+                    <div>
+                        <button id="decremento" class="btn-pequeno color-secundario-fondo color-texto-blanco" onclick="cambiarValor(this)">-</button>
+                        <input type="number" min="0" max="10" value="0" step="1" value="0" id="cantidadProducto" readonly>
+                        <button id="incremento" class="btn-pequeno color-secundario-fondo color-texto-blanco" onclick="cambiarValor(this)">+</button>
+                    </div>
+                </div>    
+            </div>
+        `
+    })
+}
+
+function cambiarValor(btn) {
+    const cantidadProducto = document.getElementById('cantidadProducto');
+    let id = btn.getAttribute("id")
+    let min = cantidadProducto.getAttribute("min");
+    let max = cantidadProducto.getAttribute("max");
+    let step = cantidadProducto.getAttribute("step");
+    let val = cantidadProducto.getAttribute("value");
+    let calcStep = (id == "incremento") ? (step * 1) : (step * -1);
+    let nvoValor = parseInt(val) + calcStep;
+
+    if (nvoValor >= min && nvoValor <= max) {
+        cantidadProducto.setAttribute("value", nvoValor);
+    }
+}
+
+function verCarrito() {
+    document.getElementById('paginaCategorias').style.display = 'none';
+    document.getElementById('paginaProductos').style.display = 'none';
+    document.getElementById('paginaEmpresas').style.display = 'none';
+    document.getElementById('carrito').style.display = 'block';
+
+    document.getElementById('iconoCarrito').style.display = 'none';
+}
+
+function direccion() {
+    document.getElementById('carrito').style.display = 'none';
+    document.getElementById('direccion').style.display = 'block';
+}
+
+function regresarACarrito() {
+    document.getElementById('paginaCategorias').style.display = 'none';
+    document.getElementById('paginaProductos').style.display = 'none';
+    document.getElementById('paginaEmpresas').style.display = 'none';
+    document.getElementById('direccion').style.display = 'none';
+    document.getElementById('carrito').style.display = 'block';
+}
+
+function metodoDePago() {
+    document.getElementById('direccion').style.display = 'none';
+    document.getElementById('metodoDePago').style.display = 'block';
+}
+
+function regresarADireccion() {
+    document.getElementById('metodoDePago').style.display = 'none';
+    document.getElementById('direccion').style.display = 'block';
 }
